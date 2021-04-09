7fc9f8e04366618c0521ad14882fdde2365e49ba
Eduardo Luiz <hello@eduardoluiz.dev>
2021-04-09 11:20:24 -0300

    feat: created continents slide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,43 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+
 import { Header } from "../components/Header";
+import { Slides } from "../components/Slides";
+
+type SlideItemProps = {
+  id: number;
+  image: string;
+  name: string;
+  summary: string;
+  url: string;
+};
+
+interface ContinentsProps extends SlideItemProps {
+  continents: Array<SlideItemProps>;
+}
 
 export default function Home() {
+  const [continents, setContinents] = useState<ContinentsProps[]>([]);
+
+  useEffect(() => {
+    async function loadContinents() {
+      api.get("/continents").then((response) => {
+        console.log(response.data);
+        setContinents(response.data);
+      });
+    }
+
+    loadContinents();
+  }, []);
+
   return (
     <>
       <Head>
         <title>Wordtrip | Home</title>
       </Head>
       <Header />
+      <Slides continents={continents} />
     </>
   );
 }
